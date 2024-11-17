5f9bdab32b8d23952d501e4b56e2b46a90a01403
dereck-jun <116302666+dereck-jun@users.noreply.github.com>
2024-11-17 15:41:53 +0900

    Backend Initial commit

---
+++ backend/procrastination/src/main/java/com/procrastination/ProcrastinationApplication.java
@@ -0,0 +1,13 @@
+package com.procrastination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrastinationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcrastinationApplication.class, args);
+    }
+
+}
---
+++ backend/procrastination/src/test/java/com/procrastination/ProcrastinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procrastination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcrastinationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
